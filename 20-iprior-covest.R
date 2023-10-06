060135885c99272d73994bc5515da5792f87da65
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2023-10-06 16:30:34 +0800

    Some work today

--- 20-iprior-covest.R
+++ 20-iprior-covest.R
@@ -1,4 +1,20 @@
 library(iprior)
-source("10-data.R")
+# source("10-data.R")
+set.seed(123)
 
-loglik
+# Generate data
+m <- 10  
+n <- 250  
+x <- rchisq(m, df = 100)
+Theta_true <- solve(x %*% t(x) + diag(rep(0.0001, m)))
+# R <- tcrossprod(rnorm(m, sd = 0.5))
+Theta <- Theta_true #+ R + diag(rep(0.1, m))
+y <- mvtnorm::rmvnorm(n, rep(0, m), solve(Theta))
+
+# Posterior distribution of Theta
+S <- crossprod(y - mean(y)) / n
+W0 <- 1000 * diag(m)
+nu <- m
+What <- solve(W0) + n * S
+nuhat <- nu + n
+Thetahat <- nuhat * solve(What) 
